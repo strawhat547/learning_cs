cb6f4c1ca43a17c7c6a242fd7e945de38c15e644
strawhat547 <yinyangstrawhat@gmail.com>
2023-10-19 20:52:57 +0800

    iteration notation

---
+++ iteration.js
@@ -0,0 +1,60 @@
+/**
+ * iteration
+ * https://en.wikipedia.org/wiki/Iteration
+ */
+
+/**
+ * 
+ * @param {Array} nums 
+ */
+function print_arr(nums) {
+    let len = nums.length
+    for(let i = 0; i < len; i++) {
+        console.log(nums[i]);
+    }
+}
+
+print_arr([1,5,6,7,7])
+
+/**
+ * 1
+ * 5
+ * 6
+ * 7
+ * 7
+ */
+
+
+/**
+ * 1..100求和
+ * @param {number} num 
+ * @returns sum
+ */
+function sum(num) {
+    let sum = 0
+    for(let i = 1; i <= num; i++) sum += i
+    return sum
+}
+
+console.log(sum(100));
+
+//5050
+
+/**
+ * https://en.wikipedia.org/wiki/Fibonacci_sequence
+ * @param {number} num 
+ * @returns number
+ */
+function fibobacci(num) {
+    let sequence = [0, 1]
+    for(let i = 2; i <= num; i++) sequence.push(sequence[i -1] + sequence[i - 2])
+    return sequence[num]
+}
+
+console.log(fibobacci(5));
+
+//5
+
+
+
+
