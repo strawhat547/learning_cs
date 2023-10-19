bf217a688b054569daf34d4d53238c9542cde9c0
strawhat547 <yinyangstrawhat@gmail.com>
2023-10-19 19:53:20 +0800

    complement example

--- recursion.js
+++ recursion.js
@@ -1,6 +1,9 @@
 
 
-
+/**
+ * 
+ * @param {*} num 
+ */
 function print_num(num) {
     console.log(num) 
     if(num < 4) {
@@ -11,3 +14,16 @@ function print_num(num) {
 print_num(0)
 
 console.log('*******************');
+
+/**
+ * 
+ * @param {*} num 
+ */
+function print_nums(num) {
+    if(num < 4) {
+        print_nums(num + 1)
+    }
+    console.log(num);
+}
+
+print_nums(0)
