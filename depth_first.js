79b62704adf08dbfdcba8f91c15d1e5d11ce2c25
strawhat547 <yinyangstrawhat@gmail.com>
2023-10-19 20:10:55 +0800

    depth first recursion

---
+++ depth_first.js
@@ -0,0 +1,41 @@
+function TreeNode(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+}
+
+/**
+ * 前序遍历
+ * @param {TreeNode} root 
+ * @returns 
+ */
+function pre_order_traverse(root) {
+    if(!root) return
+    console.log(root.val)
+    pre_order_traverse(root.lefet)
+    pre_order_traverse(root.right)
+}
+
+/**
+ * 中序遍历
+ * @param {TreeNode} root 
+ * @returns 
+ */
+function in_order_traverse(root) {
+    if(!root) return
+    in_order_traverse(root.left)
+    console.log(root.val);
+    in_order_traverse(root.right)
+}
+
+/**
+ * 后序遍历
+ * @param {TreeNode} root 
+ * @returns 
+ */
+function post_order_traverse(root) {
+    if(!root) return
+    post_order_traverse(root.left)
+    post_order_traverse(root.right)
+    console.log(root.val)
+}
